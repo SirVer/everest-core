cfcd05a4f450b840774ae02740390233ea8688ea
Dima Dorezyuk <ddo@qwello.eu>
2023-10-04 09:40:05 +0200

    Change the interface

--- modules/rust_examples/RsExampleUser/src/eventually_generated.rs
+++ modules/rust_examples/RsExampleUser/src/eventually_generated.rs
@@ -1,18 +1,14 @@
+use everestrs::{Publisher, Subscriber};
 use std::collections::HashMap;
+use std::sync::Arc;
 
-/// This interface defines an example_user interface that uses the example interface
-pub trait ExampleUserService {}
-
-pub trait ExampleSubscriber {
-    fn on_max_current(&self, value: f64);
+/// The publisher for the example module.
+pub struct ExamplePublisher<'a> {
+    runtime: &'a everestrs::Runtime,
+    name: &'a str,
 }
 
-/// This interface defines an example interface that uses multiple framework features
-pub struct ExampleClient {
-    raw_publisher: everestrs::RawPublisher,
-}
-
-impl ExampleClient {
+impl<'a> ExamplePublisher<'a> {
     /// This command checks if something is stored under a given key
     ///
     /// `key`: Key to check the existence for
@@ -23,7 +19,7 @@ impl ExampleClient {
             "key": key,
         });
         let blob = self
-            .raw_publisher
+            .runtime
             .call_command("their_example", "uses_something", &args);
         let return_value: bool = ::serde_json::from_value(blob)
             .map_err(|_| everestrs::Error::InvalidArgument("return_value"))?;
@@ -31,43 +27,66 @@ impl ExampleClient {
     }
 }
 
-pub trait Module: Sized {
-    fn on_ready(&self) {}
-    fn main(&self) -> &dyn ExampleUserService;
-    fn their_example_subscriber(&self) -> &dyn ExampleSubscriber;
+pub trait ExampleSubscriber: Sync + Send {
+    fn on_max_current(&self, pub_impl: &ExamplePublisher, value: f64);
 }
 
-/// We want the user to just implement the `Module` trait above to get access to everything that
-/// EVerest has to offer, however for the `everestrs` library, we have to implement the
-/// `GenericModule`. This thin wrapper does the translation between the generic module and the
-/// specific implementation provided by the user.
-pub struct GenericToSpecificModuleProxy<T: Module>(T);
+pub trait OnReadySubscriber: Sync + Send {
+    fn on_ready(&self);
+}
 
-impl<T: Module> everestrs::GenericModule for GenericToSpecificModuleProxy<T> {
-    #[allow(unused_variables)]
+/// The struct holding the subscriber implementation.
+struct ModuleSubscriber {
+    /// The server subscribers.
+    their_example: Arc<dyn ExampleSubscriber>,
+    another_example: Arc<dyn ExampleSubscriber>,
+
+    /// The on-ready subscriber.
+    on_ready: Arc<dyn OnReadySubscriber>,
+}
+
+impl Subscriber for ModuleSubscriber {
     fn handle_command(
         &self,
+        rt: &everestrs::Runtime,
         implementation_id: &str,
-        cmd_name: &str,
+        name: &str,
         parameters: HashMap<String, serde_json::Value>,
-    ) -> ::everestrs::Result<serde_json::Value> {
-        match implementation_id {
-            _ => Err(everestrs::Error::InvalidArgument(
-                "Unknown implementation_id called.",
-            )),
-        }
+    ) -> everestrs::Result<serde_json::Value> {
+        todo!()
     }
 
-    #[allow(unused_variables)]
     fn handle_variable(
         &self,
+        rt: &everestrs::Runtime,
         implementation_id: &str,
         name: &str,
         value: serde_json::Value,
-    ) -> ::everestrs::Result<()> {
+    ) -> everestrs::Result<()> {
         match implementation_id {
             "their_example" => {
-                their_example::handle_variable(self.0.their_example_subscriber(), name, value)
+                let pub_impl = ExamplePublisher {
+                    runtime: rt,
+                    name: "their_example",
+                };
+                example_interface::handle_variable(
+                    &pub_impl,
+                    self.their_example.as_ref(),
+                    name,
+                    value,
+                )
+            }
+            "another_example" => {
+                let pub_impl = ExamplePublisher {
+                    runtime: rt,
+                    name: "another_example",
+                };
+                example_interface::handle_variable(
+                    &pub_impl,
+                    self.another_example.as_ref(),
+                    name,
+                    value,
+                )
             }
             _ => Err(everestrs::Error::InvalidArgument(
                 "Unknown variable received.",
@@ -76,23 +95,97 @@ impl<T: Module> everestrs::GenericModule for GenericToSpecificModuleProxy<T> {
     }
 
     fn on_ready(&self) {
-        self.0.on_ready()
+        todo!()
     }
 }
 
-pub fn init_from_commandline<T: Module + 'static>(
-    init_module: impl FnOnce(ExampleClient) -> T,
-) -> everestrs::Runtime {
-    everestrs::Runtime::from_commandline(|raw_publisher| {
-        let their_example_client = ExampleClient { raw_publisher };
-        let specific_module = init_module(their_example_client);
-        GenericToSpecificModuleProxy(specific_module)
-    })
+pub struct Module {
+    mod_impl: everestrs::Runtime,
 }
 
-mod their_example {
-    pub(super) fn handle_variable(
-        their_example_subscriber: &dyn super::ExampleSubscriber,
+impl Module {
+    pub fn new(
+        their_example: Arc<dyn ExampleSubscriber>,
+        another_example: Arc<dyn ExampleSubscriber>,
+        on_ready: Arc<dyn OnReadySubscriber>,
+    ) -> Self {
+        let sub_impl = Box::new(ModuleSubscriber {
+            their_example,
+            another_example,
+            on_ready,
+        });
+
+        Self {
+            mod_impl: everestrs::Runtime::from_commandline(sub_impl),
+        }
+    }
+}
+
+// pub trait Module: Sized {
+//     fn on_ready(&self) {}
+//     fn main(&self) -> &dyn ExampleUserService;
+//     fn their_example_subscriber(&self) -> &dyn ExampleSubscriber;
+// }
+
+// We want the user to just implement the `Module` trait above to get access to everything that
+// EVerest has to offer, however for the `everestrs` library, we have to implement the
+// `GenericModule`. This thin wrapper does the translation between the generic module and the
+// specific implementation provided by the user.
+// pub struct GenericToSpecificModuleProxy<T: Module>(T);
+
+// impl<T: Module> everestrs::GenericModule for GenericToSpecificModuleProxy<T> {
+//     #[allow(unused_variables)]
+//     fn handle_command(
+//         &self,
+//         implementation_id: &str,
+//         cmd_name: &str,
+//         parameters: HashMap<String, serde_json::Value>,
+//     ) -> ::everestrs::Result<serde_json::Value> {
+//         match implementation_id {
+//             _ => Err(everestrs::Error::InvalidArgument(
+//                 "Unknown implementation_id called.",
+//             )),
+//         }
+//     }
+
+//     #[allow(unused_variables)]
+//     fn handle_variable(
+//         &self,
+//         implementation_id: &str,
+//         name: &str,
+//         value: serde_json::Value,
+//     ) -> ::everestrs::Result<()> {
+//         match implementation_id {
+//             "their_example" => {
+//                 their_example::handle_variable(self.0.their_example_subscriber(), name, value)
+//             }
+//             _ => Err(everestrs::Error::InvalidArgument(
+//                 "Unknown variable received.",
+//             )),
+//         }
+//     }
+
+//     fn on_ready(&self) {
+//         self.0.on_ready()
+//     }
+// }
+
+// pub fn init_from_commandline<T: Module + 'static>(
+//     init_module: impl FnOnce(ExampleClient) -> T,
+// ) -> everestrs::Runtime {
+//     everestrs::Runtime::from_commandline(|raw_publisher| {
+//         let their_example_client = ExampleClient { raw_publisher };
+//         let specific_module = init_module(their_example_client);
+//         GenericToSpecificModuleProxy(specific_module)
+//     })
+// }
+
+mod example_interface {
+    // use everestrs::Runtime;
+
+    pub(super) fn handle_variable<T: super::ExampleSubscriber + ?Sized>(
+        pub_impl: &super::ExamplePublisher,
+        their_example_subscriber: &T,
         name: &str,
         value: serde_json::Value,
     ) -> ::everestrs::Result<()> {
@@ -100,7 +193,7 @@ mod their_example {
             "max_current" => {
                 let v: f64 = ::serde_json::from_value(value)
                     .map_err(|_| everestrs::Error::InvalidArgument("max_current"))?;
-                their_example_subscriber.on_max_current(v);
+                their_example_subscriber.on_max_current(pub_impl, v);
                 Ok(())
             }
             _ => Err(everestrs::Error::InvalidArgument(
--- modules/rust_examples/RsExampleUser/src/main.rs
+++ modules/rust_examples/RsExampleUser/src/main.rs
@@ -1,44 +1,57 @@
+use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
 mod eventually_generated;
 
-pub struct Module {
-    their_example_client: eventually_generated::ExampleClient,
+struct ExampleClient {
+    max_current: Mutex<Option<f64>>,
 }
 
-impl eventually_generated::ExampleUserService for Module {}
-
-impl eventually_generated::ExampleSubscriber for Module {
-    fn on_max_current(&self, value: f64) {
-        println!("Received max_current: {value}");
+impl ExampleClient {
+    fn new() -> Self {
+        Self {
+            max_current: Mutex::new(None),
+        }
     }
 }
 
-impl eventually_generated::Module for Module {
-    fn main(&self) -> &dyn eventually_generated::ExampleUserService {
-        self
+impl eventually_generated::ExampleSubscriber for ExampleClient {
+    fn on_max_current(&self, pub_impl: &eventually_generated::ExamplePublisher, value: f64) {
+        println!("Received the value {value}");
+        let _ = pub_impl.uses_something("hello_there".to_string());
+        *self.max_current.lock().unwrap() = Some(value);
     }
+}
 
-    fn their_example_subscriber(&self) -> &dyn eventually_generated::ExampleSubscriber {
-        self
-    }
+struct Module {
+    their_example: Arc<ExampleClient>,
+    another_example: Arc<ExampleClient>,
+    min_current: Mutex<Option<f64>>,
+}
 
+impl eventually_generated::OnReadySubscriber for Module {
     fn on_ready(&self) {
-        // Ignoring any potential errors here.
-        let _ = self
-            .their_example_client
-            .uses_something("hello_there".to_string());
+        let mut their_current = self.their_example.max_current.lock().unwrap();
+        let mut another_current = self.another_example.max_current.lock().unwrap();
+        *their_current = Some(1.);
+        *another_current = Some(2.);
+        // uses somehow both...
+        *self.min_current.lock().unwrap() = Some(1.);
     }
 }
 
 fn main() {
-    let _mod = eventually_generated::init_from_commandline(|their_example_client| Module {
-        their_example_client,
+    let their_example = Arc::new(ExampleClient::new());
+    let another_example = Arc::new(ExampleClient::new());
+    let module = Arc::new(Module {
+        their_example: their_example.clone(),
+        another_example: another_example.clone(),
+        min_current: Mutex::new(None),
     });
-
-    // Everest is driving execution in the background for us, nothing to do.
-    loop {
-        let dt = time::Duration::from_millis(250);
-        thread::sleep(dt);
-    }
+    let runtime = eventually_generated::Module::new(
+        their_example.clone(),
+        another_example.clone(),
+        module.clone(),
+    );
+    
 }
