5ba9b5a23a79c4bb9c019c29c21e262406f8b4c3
Dima Dorezyuk <ddo@qwello.eu>
2023-10-06 16:00:09 +0200

    wip

--- modules/rust_examples/RsExampleUser/src/eventually_generated.rs
+++ modules/rust_examples/RsExampleUser/src/eventually_generated.rs
@@ -1,106 +1,67 @@
-use everestrs::{Publisher, Subscriber};
+use everestrs::{Error, Result, RuntimePublisher, RuntimeSubscriber, Subscriber};
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
-/// The publisher for the example module.
-pub struct ExamplePublisher<'a> {
-    runtime: &'a everestrs::Runtime,
-    name: &'a str,
+/// The publisher for the example module. The class is clone-able and just holds
+/// a shared-ptr to the cpp implementation.
+#[derive(Clone)]
+pub struct ExamplePublisher {
+    runtime: RuntimePublisher,
 }
 
-impl<'a> ExamplePublisher<'a> {
+impl ExamplePublisher {
     /// This command checks if something is stored under a given key
     ///
     /// `key`: Key to check the existence for
     ///
-    /// Returns: Returns 'True' if something was stored for this key*/
-    pub fn uses_something(&self, key: String) -> ::everestrs::Result<bool> {
+    /// Returns: Returns 'True' if something was stored for this key
+    pub fn uses_something(&self, key: String) -> Result<bool> {
         let args = serde_json::json!({
             "key": key,
         });
         let blob = self
             .runtime
             .call_command("their_example", "uses_something", &args);
-        let return_value: bool = ::serde_json::from_value(blob)
-            .map_err(|_| everestrs::Error::InvalidArgument("return_value"))?;
+        let return_value: bool =
+            ::serde_json::from_value(blob).map_err(|_| Error::InvalidArgument("return_value"))?;
         Ok(return_value)
     }
 }
 
+/// The collection of all publishers for this module.
+#[derive(Clone)]
+pub struct ModulePublisher {
+    pub their_publisher: ExamplePublisher,
+    pub another_publisher: ExamplePublisher,
+}
+
+/// Trait for the user to implement.
 pub trait ExampleSubscriber: Sync + Send {
-    fn on_max_current(&self, pub_impl: &ExamplePublisher, value: f64);
+    fn on_max_current(&self, pub_impl: &ModulePublisher, value: f64);
 }
 
+/// Trait for the user to implement.
 pub trait OnReadySubscriber: Sync + Send {
-    fn on_ready(&self);
+    fn on_ready(&self, pub_impl: &ModulePublisher);
 }
 
-/// The struct holding the subscriber implementation.
-struct ModuleSubscriber {
-    /// The server subscribers.
+/// The struct holding everything necessary for the module to run.
+///
+/// If the user may drop the provided subscriber and the code will still work.
+/// The user may furthermore drop the [Module] - in this case we will stop
+/// receiving callbacks - however the cloned publisher will continue to work
+/// until dropped.
+pub struct Module {
+    /// All subscribers.
     their_example: Arc<dyn ExampleSubscriber>,
     another_example: Arc<dyn ExampleSubscriber>,
-
-    /// The on-ready subscriber.
     on_ready: Arc<dyn OnReadySubscriber>,
-}
 
-impl Subscriber for ModuleSubscriber {
-    fn handle_command(
-        &self,
-        rt: &everestrs::Runtime,
-        implementation_id: &str,
-        name: &str,
-        parameters: HashMap<String, serde_json::Value>,
-    ) -> everestrs::Result<serde_json::Value> {
-        todo!()
-    }
+    /// The publisher.
+    publisher: ModulePublisher,
 
-    fn handle_variable(
-        &self,
-        rt: &everestrs::Runtime,
-        implementation_id: &str,
-        name: &str,
-        value: serde_json::Value,
-    ) -> everestrs::Result<()> {
-        match implementation_id {
-            "their_example" => {
-                let pub_impl = ExamplePublisher {
-                    runtime: rt,
-                    name: "their_example",
-                };
-                example_interface::handle_variable(
-                    &pub_impl,
-                    self.their_example.as_ref(),
-                    name,
-                    value,
-                )
-            }
-            "another_example" => {
-                let pub_impl = ExamplePublisher {
-                    runtime: rt,
-                    name: "another_example",
-                };
-                example_interface::handle_variable(
-                    &pub_impl,
-                    self.another_example.as_ref(),
-                    name,
-                    value,
-                )
-            }
-            _ => Err(everestrs::Error::InvalidArgument(
-                "Unknown variable received.",
-            )),
-        }
-    }
-
-    fn on_ready(&self) {
-        self.on_ready.on_ready();
-    }
-}
-
-pub struct Module {
-    mod_impl: everestrs::Runtime,
+    /// The handle to the subscriber runtime.
+    runtime: Mutex<Option<RuntimeSubscriber>>,
 }
 
 impl Module {
@@ -108,38 +69,88 @@ impl Module {
         their_example: Arc<dyn ExampleSubscriber>,
         another_example: Arc<dyn ExampleSubscriber>,
         on_ready: Arc<dyn OnReadySubscriber>,
-    ) -> Self {
-        let sub_impl = Box::new(ModuleSubscriber {
+    ) -> Arc<Self> {
+        let runtime = RuntimePublisher::new();
+        let publisher = ModulePublisher {
+            their_publisher: ExamplePublisher {
+                runtime: runtime.clone(),
+            },
+            another_publisher: ExamplePublisher {
+                runtime: runtime.clone(),
+            },
+        };
+
+        let this = Arc::new(Self {
             their_example,
             another_example,
             on_ready,
+            publisher: publisher,
+            runtime: Mutex::new(None), // module: runtime.clone(),
         });
+        let weak_this = Arc::<Module>::downgrade(&this);
+        *this.runtime.lock().unwrap() = Some(RuntimeSubscriber::new(&runtime, weak_this));
 
-        Self {
-            mod_impl: everestrs::Runtime::from_commandline(sub_impl),
+        this
+    }
+}
+
+impl Subscriber for Module {
+    fn handle_command(
+        &self,
+        // rt: &Runtime,
+        implementation_id: &str,
+        name: &str,
+        parameters: HashMap<String, serde_json::Value>,
+    ) -> Result<serde_json::Value> {
+        todo!()
+    }
+
+    fn handle_variable(
+        &self,
+        implementation_id: &str,
+        name: &str,
+        value: serde_json::Value,
+    ) -> Result<()> {
+        match implementation_id {
+            "their_example" => example_interface::handle_variable(
+                &self.publisher,
+                self.their_example.as_ref(),
+                name,
+                value,
+            ),
+            "another_example" => example_interface::handle_variable(
+                &self.publisher,
+                self.another_example.as_ref(),
+                name,
+                value,
+            ),
+            _ => Err(Error::InvalidArgument("Unknown variable received.")),
         }
+    }
+
+    fn on_ready(&self) {
+        self.on_ready.on_ready(&self.publisher)
     }
 }
 
 mod example_interface {
-    // use everestrs::Runtime;
+    // use Runtime;
+    use super::*;
 
     pub(super) fn handle_variable<T: super::ExampleSubscriber + ?Sized>(
-        pub_impl: &super::ExamplePublisher,
+        pub_impl: &super::ModulePublisher,
         their_example_subscriber: &T,
         name: &str,
         value: serde_json::Value,
-    ) -> ::everestrs::Result<()> {
+    ) -> Result<()> {
         match name {
             "max_current" => {
                 let v: f64 = ::serde_json::from_value(value)
-                    .map_err(|_| everestrs::Error::InvalidArgument("max_current"))?;
+                    .map_err(|_| Error::InvalidArgument("max_current"))?;
                 their_example_subscriber.on_max_current(pub_impl, v);
                 Ok(())
             }
-            _ => Err(everestrs::Error::InvalidArgument(
-                "Unknown variable received.",
-            )),
+            _ => Err(Error::InvalidArgument("Unknown variable received.")),
         }
     }
 }
--- modules/rust_examples/RsExampleUser/src/main.rs
+++ modules/rust_examples/RsExampleUser/src/main.rs
@@ -5,21 +5,32 @@ mod eventually_generated;
 
 struct ExampleClient {
     max_current: Mutex<Option<f64>>,
+    thread: Mutex<Option<thread::JoinHandle<()>>>,
 }
 
 impl ExampleClient {
     fn new() -> Self {
         Self {
             max_current: Mutex::new(None),
+            thread: Mutex::new(None),
         }
     }
 }
 
 impl eventually_generated::ExampleSubscriber for ExampleClient {
-    fn on_max_current(&self, pub_impl: &eventually_generated::ExamplePublisher, value: f64) {
+    fn on_max_current(&self, pub_impl: &eventually_generated::ModulePublisher, value: f64) {
         println!("Received the value {value}");
-        let _ = pub_impl.uses_something("hello_there".to_string());
+        let _ = pub_impl
+            .their_publisher
+            .uses_something("hello_there".to_string());
         *self.max_current.lock().unwrap() = Some(value);
+
+        // Example where we start a thread with the publisher. The cloning is
+        // only done if the user wants to offload the publisher into a thread.
+        let clone = pub_impl.clone();
+        *self.thread.lock().unwrap() = Some(thread::spawn(move || {
+            let _ = clone.another_publisher.uses_something("foo".to_string());
+        }))
     }
 }
 
@@ -30,7 +41,7 @@ struct Module {
 }
 
 impl eventually_generated::OnReadySubscriber for Module {
-    fn on_ready(&self) {
+    fn on_ready(&self, _pub_impl: &eventually_generated::ModulePublisher) {
         let mut their_current = self.their_example.max_current.lock().unwrap();
         let mut another_current = self.another_example.max_current.lock().unwrap();
         *their_current = Some(1.);
@@ -48,10 +59,9 @@ fn main() {
         another_example: another_example.clone(),
         min_current: Mutex::new(None),
     });
-    let runtime = eventually_generated::Module::new(
+    let _ = eventually_generated::Module::new(
         their_example.clone(),
         another_example.clone(),
         module.clone(),
     );
-    
 }
